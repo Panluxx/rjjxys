cdce638b2a66f70f0b925604537f37ad54e1252b
panlu <619414118@qq.com>
2022-11-02 16:13:19 +0800

    新增截图

--- TestCases/test_login.py
+++ TestCases/test_login.py
@@ -35,14 +35,14 @@ class TestLogin(BasePage):
             self.double_click(get_path('清空密码'), img_doc='双击密码输入框')
         else:
             # 密码输入框
-            self.touch(get_path('输入密码'), img_doc='输入密码')
-        self.text(password, img_doc=f'输入用户名{password}')
+            self.touch(get_path('输入密码'), img_doc='点击密码输入框')
+        self.text(password, img_doc=f'输入密码{password}')
         # 记住密码
         if self.exists(get_path('未勾选'), img_doc='记住密码未勾选'):
-            self.touch(get_path('未勾选'), img_doc='勾选密码')
+            self.touch(get_path('未勾选'), img_doc='勾选记住密码')
         # 勾选协议
         if self.exists(get_path('未勾选'), img_doc='记住密码未勾选'):
-            self.touch(get_path('未勾选'), img_doc='勾选协议')
+            self.touch(get_path('未勾选'), img_doc='勾选并同意协议')
         # 登录
         self.touch(get_path('登录'), img_doc='登录')
         self.assert_exists(filepath=get_path('首页logo'), msg="登录成功后_存在logo", img_doc='检查首页logo')
